d6fe76579b3aba40e959db8bbc710aa9e792ebee
Pau Hinojosa Leiva <pau.hinojosa@students.salle.url.edu>
2022-01-09 21:20:55 +0100

    ñ

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -8,6 +8,33 @@ var app = new Vue({
         heroes: "",
         searchInput: ""
       };
+    },
+    methods: {
+  
+      loadHeroeData: function () {
+  
+            fetch('http://localhost:5500/api/list?search=' + this.searchInput)
+            .then(response => response.json())
+            .then(datos => {
+              
+              this.heroes = datos;
+              
+              for (var i = 0; i< this.heroes.length; i++) {
+                this.heroes[i].flag_big = false;
+              }
+              
+              console.log(this.heroes );
+            })
+            .catch(err => {
+              console.log('Error: '+ err);
+            });
+           // console.log('Valor pasado:' + this.$parent.heroes);
+            
+      },
+      greet: function (event) {
+        // `this` inside methods point to the Vue instance
+        alert('Hello ' + this.heroes.length + '!')
+      }
     }
   })
 
